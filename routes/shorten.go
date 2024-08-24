598a599adec0bf86b27f4127be85bfff93aedc5f
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-08-24 22:37:11 +0530

    fix: fixes doman recursive call err and enforcing http

--- routes/shorten.go
+++ routes/shorten.go
@@ -1,6 +1,12 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/2k4sm/shawty/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +20,28 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining uint          `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse json",
+		})
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid url",
+		})
+	}
+
+	if !helpers.RemoveDomainErr(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "invalid url",
+		})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
